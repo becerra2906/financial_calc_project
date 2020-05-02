7bdae63ab410cde63e728d13a3cba271b18e3baa
becerra2906 <55593696+becerra2906@users.noreply.github.com>
2020-05-02 17:53:36 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,9 @@
+# this program will calculate the monthly budget distribution based on basic personal finance
+# budgetary distributions and an users' inputted base salary
+#print('Hi, write start to begin')
+print("Hi! Welcome to your budget calculator.")
+print("What's your name?")
+user_name = input()
+#print( user_name )
+#print(" we are going to help you manage your finances better")
+#budget = input()
