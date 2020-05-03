7e84b9db69e581d088f5def9f7b1c1cc8e9f7e51
becerra2906 <55593696+becerra2906@users.noreply.github.com>
2020-05-02 20:57:43 -0500

    V1
    Simple budget calculator

--- code.py
+++ code.py
@@ -1,9 +1,37 @@
-# this program will calculate the monthly budget distribution based on basic personal finance
-# budgetary distributions and an users' inputted base salary
-#print('Hi, write start to begin')
+# Simple budget calculator  V1.0
+# Written by : Alejandro Becerra
+
+# This small python script will help you calculate a financially healthy
+# distribution for your income, providing information for your monthly budget
+
+# variables
+monthly_rent_percentage = 0.3
+monthly_savings_percentage = 0.1
+monthly_living_expenses_percentage = 0.2
+monthly_utilities_expenses_percentage = 0.2
+monthly_leisure_expenses_percentage = 0.2
+# list for operations
+pct = ["monthly_rent_percentage", "monthly_savings_percentage", "monthly_living_expenses_percentage",
+       "monthly_utilities_expenses_percentage ", "monthly_leisure_expenses_percentage"]
+
 print("Hi! Welcome to your budget calculator.")
-print("What's your name?")
-user_name = input()
-#print( user_name )
-#print(" we are going to help you manage your finances better")
-#budget = input()
+# user_name= str(input("What's your name? "))
+# print("Cool "+ name)
+print('We are going to help you manage your finances better.')
+budget = float(input("What's your monthly income? "))
+print("""Excellent. The recommended proportion to be spend in different areas of life ensuring you maintain
+good financial health are 30% for rent, 10% for savings, 20% for groceries, 20% percent for utilities and 20% for 
+leisure.""")
+print("Thus the recommended distribution for your monthly budget is:")3
+print("For rent: ")
+print(budget * monthly_rent_percentage)
+print("For savings: ")
+print(budget*monthly_savings_percentage)
+print("For groceries: ")
+print(budget*monthly_living_expenses_percentage)
+print("For utilities: ")
+print(budget*monthly_utilities_expenses_percentage)
+print("For leisure: ")
+print(budget*monthly_leisure_expenses_percentage)
+print("----")
+print("Thank you for using our program! See you next month.")
